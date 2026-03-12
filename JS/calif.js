675866a52d121077330cd36c231075ced3e1fe4e
MrTechX <82182805+Mr-TechX@users.noreply.github.com>
2026-03-12 17:13:14 -0600

    Add alert for grade publication delay
    Added an alert message regarding grade publication delays.

--- JS/calif.js
+++ JS/calif.js
@@ -1,6 +1,9 @@
 if (sessionStorage.getItem("auth") !== "true") {
   window.location.href = "login.html";
 }
+
+alert("Lamentamos la demora de subida de calificaciones unidad uno, tuvimos dificultades tecnicas, agradecemos su comprension, las calificaciones unidad 2, seran publicadas muy pronto. | Coordinacion ISC");
+
 function toggleMenu() {
   const m = document.getElementById("mobileMenu");
   m.style.display = m.style.display === "flex" ? "none" : "flex";
